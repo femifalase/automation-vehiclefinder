cef8aa722ad4a6a5b729c16e9219ca010f945221
Richard <richard.owusu>
2017-10-19 01:07:37 +0100

    Configured extra browser: IE & Firefox

--- src/main/java/com/richard/cordbyte/base/Driver.java
+++ src/main/java/com/richard/cordbyte/base/Driver.java
@@ -1,7 +1,10 @@
 package com.richard.cordbyte.base;
 
+import com.richard.cordbyte.utils.ReadFileData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,20 +12,54 @@ public class Driver {
 
     private static WebDriver driver = null;
 
-    static String driverPath = "drivers/";
+    private final static String driverPath = "drivers/";
 
     public WebDriver getDriver() {
 
         if (driver == null) {
 
-            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+            String browser = ReadFileData.getProperties().getProperty("browser");
 
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            if (browser.equalsIgnoreCase("chrome")) {
+
+                initChome();
+
+            } else if (browser.equalsIgnoreCase("firefox")) {
+
+                initFirefox();
+
+            } else if (browser.equalsIgnoreCase("ie")) {
+
+                initIEExplorer();
+
+            }
+
+
         }
 
         return driver;
 
+    }
+
+
+    private void initIEExplorer() {
+        System.setProperty("webdriver.ie.driver", driverPath + "IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    private void initChome() {
+        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    private void initFirefox() {
+        System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 }
